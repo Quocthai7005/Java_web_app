6153beb859884c5040013c958a53d00fee4edb89
Quocthai7005 <nguyenquocthai7005@gmail.com>
2017-10-01 20:40:13 +0700

    initial commit comment

---
+++ src/test/java/thdsoft/chatproject/ChatWebApplicationTests.java
@@ -0,0 +1,16 @@
+package thdsoft.chatproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ChatWebApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
